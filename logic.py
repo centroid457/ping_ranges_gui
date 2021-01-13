a1eef4e8499680a88e21dd3f0cf8cfadcf96c163
centroid457 <centroid@mail.ru>
2021-01-13 18:46:55 +0300

    add ability to explore HostNames

--- logic.py
+++ logic.py
@@ -11,31 +11,34 @@ import time
 from pathlib import Path
 
 access_this_module_as_import = True  # at first need true to correct assertions!
-ip_ranges_default = [("::1",), ("192.168.42.0", "192.168.43.255")]
+ip_explore_dict_default = {"hosts": ["localhost", ], "addresses": [("::1",), ("192.168.42.0", "192.168.43.255")]}
 
 class Logic:
-    def __init__(self, ip_find_ranges_tuples_list=ip_ranges_default):
+    def __init__(self, ip_explore_dict=ip_explore_dict_default):
         self.ip_ping_timewait_limit_ms = 15
         self.ip_concurrent_ping_limit = 256
 
         self.lock_maxconnections = threading.BoundedSemaphore(value=self.ip_concurrent_ping_limit)
 
-        self.apply_ranges(ip_find_ranges_tuples_list)
+        self.apply_ranges(ip_explore_dict)
         return
 
-    def apply_ranges(self, ip_ranges=None):
-        print(ip_ranges)
-        if ip_ranges is not None:
+    def apply_ranges(self, ip_data=None):
+        # print(ip_data)
+        if ip_data is not None:
             self.clear_data()
-            self.ip_find_ranges_tuples_list = ip_ranges
+
+            self.ip_explore_hosts_list = ip_data["hosts"]
+            self.ip_explore_ranges_tuple_list = ip_data["addresses"]
             self.create_data()
         return
 
     def clear_data(self):
-        self.search_is_finished = False
+        self.explore_is_finished = False
 
         # SETS/DICTS/LISTS
-        self.ip_find_ranges_tuples_list = []
+        self.ip_explore_hosts_list = []
+        self.ip_explore_ranges_tuple_list = []
         self.ip_found_info_dict = {}       # {"ip": {"mac": None, "os": None, "host": None}}
 
         # COUNTERS
@@ -43,13 +46,16 @@ class Logic:
         return
 
     def create_data(self):
-        for ip_range in self.ip_find_ranges_tuples_list:
+        for ip_hostname in self.ip_explore_hosts_list:
+            self.ping_ip_start_thread(ip_hostname)
+
+        for ip_range in self.ip_explore_ranges_tuple_list:
             self.ping_ip_range(ip_range)
 
         while threading.active_count() > 1:
             time.sleep(0.5)
 
-        self.search_is_finished = True
+        self.explore_is_finished = True
         print(self.ip_found_info_dict)
         return
 
@@ -58,19 +64,19 @@ class Logic:
         ip_current = ip_start
 
         if len(ip_range) == 1:
-            self.ping_ip_start_thread(ip=ip_current)
+            self.ping_ip_start_thread(ip_current)
         elif len(ip_range) == 2:
             ip_finish = ipaddress.ip_address(ip_range[1])
             while ip_current <= ip_finish:
                 if not ip_current.is_multicast:
-                    self.ping_ip_start_thread(ip=ip_current)
+                    self.ping_ip_start_thread(ip_current)
                     # self.ping_ip(ip_current)
                 ip_current = ip_current + 1
 
         return
 
-    def ping_ip_start_thread(self, ip=None):
-        threading.Thread(target=self.ping_ip, args=(ip,), daemon=False).start()
+    def ping_ip_start_thread(self, ip_or_name=None):
+        threading.Thread(target=self.ping_ip, args=(ip_or_name,), daemon=False).start()
         return
 
     def ping_ip(self, ip=None):
@@ -89,7 +95,6 @@ class Logic:
             self.ip_found_info_dict[ip] = {}
             self.count_found_ip += 1
         return
-
 
 
 if __name__ == '__main__':
